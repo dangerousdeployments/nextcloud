apshaw11/server

Also return the phone number and twitter handle on the API
Signed-off-by: Joas Schilling <coding@schilljs.com>

apps/settings/lib/Controller/UsersController.php
@@ -397,12 +397,16 @@ class UsersController extends Controller {
 						'avatarScope' => $data[AccountManager::PROPERTY_AVATAR]['scope'],
 						'displayname' => $data[AccountManager::PROPERTY_DISPLAYNAME]['value'],
 						'displaynameScope' => $data[AccountManager::PROPERTY_DISPLAYNAME]['scope'],
+						'phone' => $data[AccountManager::PROPERTY_PHONE]['value'],
+						'phoneScope' => $data[AccountManager::PROPERTY_PHONE]['scope'],
 						'email' => $data[AccountManager::PROPERTY_EMAIL]['value'],
 						'emailScope' => $data[AccountManager::PROPERTY_EMAIL]['scope'],
 						'website' => $data[AccountManager::PROPERTY_WEBSITE]['value'],
 						'websiteScope' => $data[AccountManager::PROPERTY_WEBSITE]['scope'],
 						'address' => $data[AccountManager::PROPERTY_ADDRESS]['value'],
 						'addressScope' => $data[AccountManager::PROPERTY_ADDRESS]['scope'],
+						'twitter' => $data[AccountManager::PROPERTY_TWITTER]['value'],
+						'twitterScope' => $data[AccountManager::PROPERTY_TWITTER]['scope'],
 						'message' => $this->l10n->t('Settings saved')
 					]
 				],

dangerousdeployments/nextcloud

apps/settings/lib/Controller/UsersController.php
@@ -398,12 +398,16 @@ class UsersController extends Controller {
 						'avatarScope' => $data[IAccountManager::PROPERTY_AVATAR]['scope'],
 						'displayname' => $data[IAccountManager::PROPERTY_DISPLAYNAME]['value'],
 						'displaynameScope' => $data[IAccountManager::PROPERTY_DISPLAYNAME]['scope'],
+						'phone' => $data[IAccountManager::PROPERTY_PHONE]['value'],
+						'phoneScope' => $data[IAccountManager::PROPERTY_PHONE]['scope'],
 						'email' => $data[IAccountManager::PROPERTY_EMAIL]['value'],
 						'emailScope' => $data[IAccountManager::PROPERTY_EMAIL]['scope'],
 						'website' => $data[IAccountManager::PROPERTY_WEBSITE]['value'],
 						'websiteScope' => $data[IAccountManager::PROPERTY_WEBSITE]['scope'],
 						'address' => $data[IAccountManager::PROPERTY_ADDRESS]['value'],
 						'addressScope' => $data[IAccountManager::PROPERTY_ADDRESS]['scope'],
+						'twitter' => $data[IAccountManager::PROPERTY_TWITTER]['value'],
+						'twitterScope' => $data[IAccountManager::PROPERTY_TWITTER]['scope'],
 						'message' => $this->l10n->t('Settings saved')
 					]
 				],